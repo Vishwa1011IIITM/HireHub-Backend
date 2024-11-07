5718b6d09c8d84ddddaf97fb8405691bc19464c5
Vishwa1011IIITM <119064986+Vishwa1011IIITM@users.noreply.github.com>
2024-11-07 18:06:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "https://hire-hub-blue.vercel.app/",
+  origin: "https://hire-hub-blue.vercel.app",
   credentials: true,
 };
 
